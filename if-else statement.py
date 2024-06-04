94be3e4f099d7505d12a345312bc2a4a35e84f52
siddesh234 <siddesh@simtekway.in>
2024-06-04 15:51:09 +0530

    Add files via upload

---
+++ if-else statement.py
@@ -0,0 +1,7 @@
+number=int(input("enter the number"))
+if number>0:
+    print("this is positive number")
+else:
+    print("this is negative number")
+    if -999>-n:
+        print("this is 3 digit negative number")
