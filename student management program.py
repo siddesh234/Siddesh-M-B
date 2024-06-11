04347920c06c597fd4bc2c2babe2913c78607190
siddesh234 <siddesh@simtekway.in>
2024-06-11 13:13:02 +0530

    Add files via upload

---
+++ student management program.py
@@ -0,0 +1,13 @@
+name=input("enter student name")
+rollno=int(input("enter roll number"))
+maths=int(input("enter maths marks"))
+science=int(input("enter science marks"))
+english=int(input("enter english marks"))
+total=maths+science+english
+percentage=(total/300)*100
+print("student name:",name)
+print("roll number:",roolno)
+print("total marks:",total)
+print("percentage:",percentage)
+
+    
